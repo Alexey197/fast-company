a58c87fc2e210d81dbd8deb42c6fad94f3253717
Alexey197 <margadonnn3@gmail.com>
2022-12-25 14:49:51 +0300

    search complete

--- src/app/components/usersList.jsx
+++ src/app/components/usersList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Pagination from './pagination'
 import {paginate} from '../utils/paginate'
 import PropTypes from 'prop-types'
@@ -7,9 +7,11 @@ import GroupList from './groupList'
 import SearchStatus from './searchStatus'
 import UsersTable from './usersTable'
 import _ from 'lodash'
+
 const UsersList = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [professions, setProfessions] = useState()
+  const [searchQuery, setSearchQuery] = useState("")
   const [selectedProf, setSelectedProf] = useState()
   const [sortBy, setSortBy] = useState({iter: 'name', order: 'asc'})
   const pageSize = 8
@@ -37,10 +39,16 @@ const UsersList = () => {
   
   useEffect(() => {
     setCurrentPage(1)
-  }, [selectedProf])
+  }, [selectedProf, searchQuery])
   
   const handleProfessionSelect = (item) => {
+    if (searchQuery !== "") setSearchQuery("")
     setSelectedProf(item)
+  }
+  
+  const handleSearchQuery = ({target}) => {
+    setSelectedProf(undefined)
+    setSearchQuery(target.value)
   }
   
   const handlePageChange = (pageIndex) => {
@@ -51,13 +59,20 @@ const UsersList = () => {
   }
   
   if (users) {
-    const filteredUsers = selectedProf
+    const filteredUsers = searchQuery
       ? users.filter(
         (user) =>
-          JSON.stringify(user.profession) ===
-          JSON.stringify(selectedProf)
+          user.name
+            .toLowerCase()
+            .indexOf(searchQuery.toLowerCase()) !== -1
       )
-      : users
+      : selectedProf
+        ? users.filter(
+          (user) =>
+            JSON.stringify(user.profession) ===
+          JSON.stringify(selectedProf)
+        )
+        : users
     
     const count = filteredUsers.length
     const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order])
@@ -86,6 +101,13 @@ const UsersList = () => {
         )}
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search..."
+            onChange={handleSearchQuery}
+            value={searchQuery}
+          />
           {count > 0 && (
             <UsersTable
               users={userCrop}

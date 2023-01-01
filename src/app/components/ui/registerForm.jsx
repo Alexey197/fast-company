83ed5cacadccaf49e1f21b618ca0ee81d130e467
Alexey197 <margadonnn3@gmail.com>
2023-01-02 01:16:57 +0300

    MultiSelectField

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -4,22 +4,26 @@ import TextField from '../common/form/textField'
 import api from '../../api'
 import SelectField from '../common/form/selectField'
 import RadioField from '../common/form/radioField'
+import MultiSelectField from '../common/form/multiSelectField'
 
 const RegisterForm = () => {
   const [data, setData] = useState({
     email: "",
     password: "",
     profession: "",
-    sex: "male"
+    sex: "male",
+    qualities: []
   })
+  const [qualities, setQualities] = useState({})
   const [professions, setProfessions] = useState([])
   const [errors, setErrors] = useState({})
   
   useEffect(() => {
     api.professions.fetchAll().then((data) => setProfessions(data))
+    api.qualities.fetchAll().then((data) => setQualities(data))
   }, [])
   
-  const handleChange = ({target}) => {
+  const handleChange = (target) => {
     setData(prevState => ({
       ...prevState,
       [target.name]: target.value
@@ -108,9 +112,16 @@ const RegisterForm = () => {
           {name: "Other", value: "other"}
         ]
       }
-      value={data.sex}
-      name="sex"
-      onChange={handleChange}
+        value={data.sex}
+        name="sex"
+        onChange={handleChange}
+        label="Выберите ваш пол "
+      />
+      <MultiSelectField
+        options={qualities}
+        onChange={handleChange}
+        name="qualities"
+        label="Выберите ваши качества"
       />
       <button
         type='submit'
